5b3bf9146417c34ee726b354a0258ba3e53402b4
Luke Miller <lukealexmiller@gmail.com>
2017-02-01 15:04:07 +0100

    again

--- installCaffeCuDNNFaster_R-CNN.sh
+++ installCaffeCuDNNFaster_R-CNN.sh
@@ -49,17 +49,15 @@ echo "LIBRARY_DIRS += /usr/local/share/" >> Makefile.config
 
 # Enable support layers written in python
 sed -i 's/# WITH_PYTHON_LAYER/WITH_PYTHON_LAYER/g' Makefile.config
-touch $HOME/.bash_login
-echo "source $HOME/.bashrc" >> $HOME/.bash_login
 
-echo "export CAFFE_ROOT=$HOME/faster_rcnn/caffe-fast-rcnn" >> $HOME/.bashrc
-source $HOME/.bashrc
-echo "export PYCAFFE_ROOT=$CAFFE_ROOT/python" >> $HOME/.bashrc
-source $HOME/.bashrc
-echo "export PYTHONPATH=$PYCAFFE_ROOT:$PYTHONPATH" >> $HOME/.bashrc
-source $HOME/.bashrc
-echo "export PATH=$CAFFE_ROOT/build/tools:$PYCAFFE_ROOT:$PATH" >> $HOME/.bashrc
-source $HOME/.bashrc
+
+touch $HOME/.bash_login
+echo "source ~/.bashrc && source ~/.profile" >> $HOME/.bash_login
+
+echo "export CAFFE_ROOT=$HOME/faster_rcnn/caffe-fast-rcnn" >> $HOME/.profile
+echo "export PYCAFFE_ROOT=$CAFFE_ROOT/python" >> $HOME/.profile
+echo "export PYTHONPATH=$PYCAFFE_ROOT:$PYTHONPATH" >> $HOME/.profile
+echo "export PATH=$CAFFE_ROOT/build/tools:$PYCAFFE_ROOT:$PATH" >> $HOME/.profile
 
 echo "$CAFFE_ROOT/build/lib" >> /etc/ld.so.conf.d/caffe.conf && ldconfig
 
