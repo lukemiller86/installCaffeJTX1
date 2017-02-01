d826090704712aca26837d83ef4e50bb0d5a3b89
Luke Miller <lukealexmiller@gmail.com>
2017-02-01 13:59:52 +0100

    more with env vars

--- CaffeEnvVars.sh
+++ CaffeEnvVars.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export CAFFE_ROOT=$HOME/faster_rcnn/caffe-fast-rcnn
 export PYCAFFE_ROOT=$CAFFE_ROOT/python
-export PYTHONPATH=$PYCAFFE_ROOT:$PYTHONPATH \
-	PATH=$CAFFE_ROOT/build/tools:$PYCAFFE_ROOT:$PATH
+export PYTHONPATH=$PYCAFFE_ROOT:$PYTHONPATH
+export PATH=$CAFFE_ROOT/build/tools:$PYCAFFE_ROOT:$PATH
